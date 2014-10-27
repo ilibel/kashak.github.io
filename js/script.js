ca2e401f9eb69ba2ccbd15afae54c0d03c8c8f60
natela <natela_07@inbox.ru>
2014-10-27 15:12:34 +0300

    Added video frames in header and bottom

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("a.animated-scroll").click(function () {
+        var elementClick = $($(this).attr("href"));
+        var destination = elementClick.size() === 0 ? 0 : elementClick.offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 300) {
+            $('#to-top').fadeIn();
+        } else {
+            $('#to-top').fadeOut();
+        }
+    });
+
+    jQuery(function(){
+        jQuery(".carousel-inner").mb_YTPlayer();
+        jQuery("#footer").mb_YTPlayer();
+    });
+});
